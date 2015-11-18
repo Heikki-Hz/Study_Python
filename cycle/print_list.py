f7fa199c5c4702a1638d1eb01e28b791ec0788e0
huangzhen <ceivehuang@163.com>
2015-11-18 16:26:03 +0800

    循环相关的简单的学习代码

---
+++ cycle/print_list.py
@@ -0,0 +1,31 @@
+#-*-coding:utf-8-*-
+
+classmates = ['Heikki', 'Tom', 'Jack']
+
+for classmate in classmates:
+    print(classmate)
+
+###forÑ­»·
+test = 0
+
+for x in range(101):
+    test += x 
+print(test)
+
+#whileÑ­»·
+sum2 = 0
+n    = 100
+
+while n > 0:
+    sum2 += n
+    n -= 1
+print(sum2)
+
+
+classmates = ['Heikki', 'Tom', 'Jack']
+
+for classmate in classmates:
+    print('Hello %s' % classmate)
+
+
+
